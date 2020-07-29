427da1ab73a5bd42a580729425647f9d3d86a2af
IzzyNety <izzy.nety@gmail.com>
2020-07-29 12:30:28 +0500

    Спинер + вывод ошибки

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './spinner.css'
+
+const Spinner = () => {
+  return (
+    <div class="loadingio-spinner-spin-u0rg3du2pgt"><div class="ldio-lu8bann34c">
+    <div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div>
+    </div></div>
+  )
+}
+
+export default Spinner;
